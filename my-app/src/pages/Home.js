3d84a5eba5293aea60ae893ed8ee79d5183488c0
“Hareesh <hareesh4000@yahoo.com>
2021-09-25 14:05:52 -0700

    Part of homepage done

---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,28 @@
+import icon from '../images/icon_pic.png';
+import background from '../images/home-background.jpg';
+import '../styles/Home.css';
+
+function Home() {
+	return (
+		<div>
+			<img className="background" src={background} resizeMode="stretch" />
+			<navigator>
+            <img className="icon" src={icon} />
+				<a href="/" className="home-button">
+					Home
+				</a>
+				<a href="/about" className="about-button">
+					About Me
+				</a>
+				<a href="/projects" className="project-button">
+					My Projects
+				</a>
+				<a href="https://www.linkedin.com/in/hareesh-pedireddi/" target="_blank" className="linkedin-button">
+					Connect With Me
+				</a>
+			</navigator>
+		</div>
+	);
+}
+
+export default Home;
