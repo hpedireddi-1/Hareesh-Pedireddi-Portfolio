8ef01951085e1a55f808151c6f7a0bd4365f8f54
“Hareesh <hareesh4000@yahoo.com>
2021-09-25 17:05:58 -0700

    Dropdown menu with submit button

--- my-app/src/pages/Home.js
+++ my-app/src/pages/Home.js
@@ -1,16 +1,17 @@
+import React from 'react';
+
 import icon from '../images/icon_pic.png';
 import background from '../images/home-background.jpg';
 import '../styles/Home.css';
-import Dropdown from 'react-dropdown';
-import 'react-dropdown/style.css';
 
 function Home() {
-	const options = [ 'checkout my résumé', 'view my projects', 'connect with me on LinkedIn' ];
+	const [ option, setOption ] = React.useState('None');
+	const options = [ 'None', 'Résumé', 'Projects', 'LinkedIn' ];
 
 	return (
 		<div>
 			<img className="background" src={background} resizeMode="stretch" />
-			<navigator>
+			<div className="navigator">
 				<img className="icon" src={icon} />
 				<a href="/" className="home-button">
 					Home
@@ -24,13 +25,33 @@ function Home() {
 				<a href="https://www.linkedin.com/in/hareesh-pedireddi/" target="_blank" className="linkedin-button">
 					Connect With Me
 				</a>
-			</navigator>
+			</div>
 			<div className="msg">
 				<div className="hello-msg">Hello, I'm</div>
 				<div className="name">Hareesh Pedireddi</div>
 				<div className="options">
 					<div className="options-msg">Would you like to: </div>
-					<Dropdown className="options-dropdown" options={options} placeholder="Select an option" />
+					<form
+						onSubmit={(event) => {
+							console.log(option);
+						}}
+					>
+						<select
+							className="options-dropdown"
+							value={option}
+							onChange={(event) => {
+								setOption(event.target.value);
+							}}
+						>
+							<option className="boldtext" value={options[0]}>
+								Select an option
+							</option>
+							<option value={options[1]}>checkout my résumé</option>
+							<option value={options[2]}>view my projects</option>
+							<option value={options[3]}>connect with me on LinkedIn</option>
+						</select>
+						<input type="submit" value="Submit" />
+					</form>
 				</div>
 			</div>
 		</div>
